4b32da8bfa42710a4c103b1c65115f9672331e69
ADMIN <chamodmadusanka012@gmail.com>
2023-06-13 20:26:42 +0530

    Final commit

---
+++ app/src/main/java/com/example/cnoteapp/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.cnoteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if (currentUser==null){
+                    startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                }else {
+                    startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                }
+
+               finish();
+            }
+        } ,1000);
+    }
+}
